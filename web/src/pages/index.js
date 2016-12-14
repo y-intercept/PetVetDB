51f92540145d72274c1ecdcad5f03ec27bc8f825
Joseph <jcdaniel55@gmail.com>
2016-12-14 14:57:01 -0500

    added .css

---
+++ web/src/pages/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+Owners: require('./owners/index'),
+OwnerForm: require('./owners/form'),
+ShowOwner: require('./owners/show'),
+Pets: require('./pets/index'),
+PetForm: require('./pets/form'),
+ShowPet: require('./pets/show'),
+Home: require('./home'),
+About: require('./about'),
+Glossary: require('./glossary/index'),
+GlossaryForm: require('./glossary/form'),
+ShowGlossary: require('./glossary/show'),
+Exams: require('./exams/index'),
+ExamForm: require('./exams/form'),
+ShowExam: require('./exams/show')
+
+}
